667b1da0ce1770c2f485e1d0b02f51f055afd27b
Omar Faruk <omarfarukcourse@gmail.com>
2025-01-02 17:15:15 +0600

    task 14 upload

---
+++ asynchronous.ts
@@ -0,0 +1,31 @@
+// Task 14: Asynchronous TypeScript and Type Aliases
+// Objective: Simulate an asynchronous operation with TypeScript.
+
+// Instructions:
+
+// Write an asynchronous function that:
+// Simulates fetching user data (containing name and age).
+// Returns the data after a short delay.
+
+type UserInfo = {
+  name: string;
+  age: number;
+};
+
+// Asynchronous function to simulate fetching user data
+async function fetchUserData(): Promise<UserInfo> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "John Doe",
+        age: 25,
+      });
+    }, 2000);
+  });
+}
+
+(async () => {
+  console.log("Fetching user data...");
+  const user = await fetchUserData();
+  console.log("User data:", user);
+})();
